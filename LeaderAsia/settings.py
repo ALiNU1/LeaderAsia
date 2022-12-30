204b2ec9341d2da7f455a1319728bff60e596dfa
alinu1 <uwu1343@gmail.com>
2022-12-30 15:16:28 +0600

    first_function

--- LeaderAsia/settings.py
+++ LeaderAsia/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,7 +48,9 @@ ROOT_URLCONF = 'LeaderAsia.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,7 +111,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- apps/settings/models.py
+++ apps/settings/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Setting(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    logo = models.ImageField(upload_to='logo/')
+    address = models.CharField(max_length=150)
+    nomer = models.CharField(max_length=100)
+    email = models.EmailField(blank=True,null=True)
+    twitter_link = models.URLField(blank=True,null=True)
+    facebook_link = models.URLField(blank=True,null=True)
+    linkedin_link = models.URLField(blank=True,null=True)
+    instagram_link = models.URLField(blank=True,null=True)
+    youtube_link = models.URLField(blank=True,null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Настройки'
+        verbose_name_plural = 'Настройки'
